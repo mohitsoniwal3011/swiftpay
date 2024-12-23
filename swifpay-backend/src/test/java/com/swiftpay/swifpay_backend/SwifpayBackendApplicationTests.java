28d8325ce55fb7a3fc5f952f70c6d36b8eb74474
Mohit Soniwal <mohit@Mohits-MacBook-Air.local>
2024-12-23 14:57:16 +0530

    initial commit

---
+++ swifpay-backend/src/test/java/com/swiftpay/swifpay_backend/SwifpayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftpay.swifpay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwifpayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
